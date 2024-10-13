0392cafdb919ebd734e3b5ed56c0b122906b1dd8
blasteg <blasteg@gmail.com>
2024-10-13 08:20:02 +0800

    LTH、LTP Data

--- MiriJoraYakus/Data/UnitGlobal.cs
+++ MiriJoraYakus/Data/UnitGlobal.cs
@@ -16,6 +16,76 @@ namespace MiriJoraYakus.Data
             new UnitData(
                  new List<CardEnum>{ CardEnum.最上静香, CardEnum.所恵美, CardEnum.ロコ, CardEnum.天空橋朋花, CardEnum.北沢志保, CardEnum.舞浜歩, CardEnum.二階堂千鶴, CardEnum.真壁瑞希, CardEnum.百瀬莉緒, CardEnum.永吉昴, CardEnum.周防桃子, CardEnum.ジュリア, CardEnum.白石紬 }
                  ,"Fairy Stars","Generic"),
+            new UnitData(
+                 new List<CardEnum>{CardEnum.伊吹翼, CardEnum.島原エレナ, CardEnum.箱崎星梨花, CardEnum.野々原茜, CardEnum.望月杏奈, CardEnum.木下ひなた, CardEnum.馬場このみ, CardEnum.大神環, CardEnum.豊川風花, CardEnum.宮尾美也, CardEnum.篠宮可憐, CardEnum.北上麗花, CardEnum.桜守歌織 }
+                 ,"Angel Stars","Generic"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.天海春香, CardEnum.最上静香, CardEnum.箱崎星梨花, CardEnum.七尾百合子, CardEnum.天空橋朋花 }
+                 ,"Legend Girls!!","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.我那覇響, CardEnum.春日未来, CardEnum.望月杏奈, CardEnum.横山奈緒, CardEnum.豊川風花 }
+                 ,"PRETTY DREAMER","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.如月千早, CardEnum.田中琴葉, CardEnum.所恵美, CardEnum.北沢志保, }
+                 ,"Blue Symphony","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.水瀬伊織, CardEnum.エミリー, CardEnum.真壁瑞希, CardEnum.百瀬莉緒 }
+                 ,"Sentimental Venus","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.星井美希, CardEnum.伊吹翼, CardEnum.北上麗花, CardEnum.ジュリア }
+                 ,"Marionetteは眠らない","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.三浦あずさ, CardEnum.高山紗代子, CardEnum.福田のり子, CardEnum.篠宮可憐 }
+                 ,"カワラナイモノ","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.高槻やよい, CardEnum.中谷育, CardEnum.矢吹可奈, CardEnum.大神環 }
+                 ,"Good-Sleep, Baby♡","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.秋月律子, CardEnum.佐竹美奈子, CardEnum.松田亜利沙, CardEnum.木下ひなた }
+                 ,"Helloコンチェルト ","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.四条貴音, CardEnum.徳川まつり, CardEnum.高坂海美, CardEnum.宮尾美也 }
+                 ,"瞳の中のシリウス ","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.菊地真, CardEnum.双海真美, CardEnum.島原エレナ, CardEnum.舞浜歩 }
+                 ,"Fu-Wa-Du-Wa","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.萩原雪歩, CardEnum.ロコ, CardEnum.二階堂千鶴, CardEnum.周防桃子 }
+                 ,"ココロがかえる場所","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.双海亜美, CardEnum.野々原茜, CardEnum.馬場このみ, CardEnum.永吉昴 }
+                 ,"Bigバルーン◎","LTP"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.高槻やよい, CardEnum.水瀬伊織, CardEnum.秋月律子, CardEnum.双海亜美, CardEnum.我那覇響 }
+                 ,"合言葉はスタートアップ！","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.春日未来, CardEnum.伊吹翼, CardEnum.望月杏奈, CardEnum.七尾百合子, CardEnum.真壁瑞希 }
+                 ,"Growing Storm!","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.最上静香, CardEnum.箱崎星梨花, CardEnum.野々原茜, CardEnum.北沢志保, CardEnum.北上麗花 }
+                 ,"Shooting Stars","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.如月千早, CardEnum.徳川まつり, CardEnum.エミリー, CardEnum.豊川風花, CardEnum.ジュリア }
+                 ,"Eternal Harmony","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.天海春香, CardEnum.松田亜利沙, CardEnum.横山奈緒, CardEnum.福田のり子, CardEnum.周防桃子 }
+                 ,"HOME, SWEET FRIENDSHIP","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.田中琴葉, CardEnum.所恵美, CardEnum.高坂海美, CardEnum.大神環, CardEnum.宮尾美也 }
+                 ,"ジレるハートに火をつけて","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.萩原雪歩, CardEnum.菊地真, CardEnum.三浦あずさ, CardEnum.舞浜歩, CardEnum.矢吹可奈 }
+                 ,"Birth of Color","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.双海真美, CardEnum.佐竹美奈子, CardEnum.中谷育, CardEnum.木下ひなた, CardEnum.馬場このみ }
+                 ,"ドリームトラベラー","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.星井美希, CardEnum.高山紗代子, CardEnum.天空橋朋花, CardEnum.二階堂千鶴, CardEnum.永吉昴 }
+                 ,"星屑のシンフォニア","LTH"),
+            new UnitData(
+                 new List<CardEnum>{ CardEnum.四条貴音, CardEnum.島原エレナ, CardEnum.ロコ, CardEnum.篠宮可憐, CardEnum.百瀬莉緒 }
+                 ,"STANDING ALIVE","LTH"),
+
         };
     }
 }
