1f6e51accf910a837580a60f8dae098a7108c288
blasteg <blasteg@gmail.com>
2024-12-28 13:45:54 +0800

    localization framework

--- MiriJoraYakus/Program.cs
+++ MiriJoraYakus/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiriJoraYakus;
+using MiriJoraYakus.Extension;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization();
+var host = builder.Build();
 
-await builder.Build().RunAsync();
+await host.SetDefaultCulture();
+
+await host.RunAsync();
